193088d7b1d1ba154176631b8a96a7d1e80e7a87
Chirag5267 <2005chiragjain@gmail.com>
2026-05-12 01:14:52 +0530

    Time: 0 ms (100%), Space: 43.3 MB (77.65%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -1,16 +1,16 @@
 class Solution {
     public int[] plusOne(int[] digits) {
         for(int i=digits.length-1; i>=0;i--){
-            if(digits[i]<9){
+            if(digits[i]<9){ //if dig less then 9
                 digits[i]++;
                 return digits;
             }
-            else{
+            else{ //if dig is 9
               digits[i]=0;  
             }
             
         }
-        int[] result=new int[digits.length+1];
+        int[] result=new int[digits.length+1]; // all dig is 9
         result[0]=1;
         return result;
         
